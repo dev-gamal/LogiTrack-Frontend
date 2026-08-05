3526f51ddcc9acdcbf5372de74b1c52ab05c1668
Gamal Badie <149597937+bd-gamal@users.noreply.github.com>
2026-08-05 13:06:46 +0100

    refactor: changing routing structure and add Layout component for better organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,44 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import RoleGuard from "./routes/RoleGuard";
 
+import Layout from "./components/Layout";
+
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Users from "./pages/Users";
 import AccessDenied from "./pages/AccessDenied";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/access-denied" element={<AccessDenied />} />
 
           <Route element={<ProtectedRoute />}>
-            <Route path="/dashboard" element={<Dashboard />} />
+            <Route element={<Layout />}>
+              <Route path="/dashboard" element={<Dashboard />} />
 
-            <Route element={<RoleGuard allowedRoles={["ADMIN"]} />}>
-              <Route path="/users" element={<Users />} />
+              <Route element={<RoleGuard allowedRoles={["ADMIN"]} />}>
+                <Route path="/users" element={<Users />} />
+              </Route>
             </Route>
           </Route>
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </AuthProvider>
